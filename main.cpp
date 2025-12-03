9cf4f153a0b1d612beaaeea3c7cd0da0da76f958
Divy481 <divysanwal0@gmail.com>
2025-12-03 17:34:47 +0530

    update

--- main.cpp
+++ main.cpp
@@ -10,9 +10,30 @@
 //error handling  
 
 
+
+int shell_built(std::vector<char*>argc,char** env,char* intial_directory){
+    if(argc.empty())return -1;
+    std::string argu = argc[0];
+
+    if(argu =="cd"){
+        if(argu.size()<2){
+            std::cerr<<"cd: missing operands\n";
+            return -1;
+        }
+        if(chdir(argc[1])!=0){
+            perror("cd");
+        }
+        return 1;
+    }else if(argu == "exit"){
+        exit(0);
+    }
+
+    return 0;
+}
 void shell_loop(char** env){
     std::string input;
     std::vector<char*> argc;
+    char * intial_directory = getcwd(NULL,0);
     while(true){
         std::cout<<"[SHELL]> ";
         if(!std::getline(std::cin,input)){
@@ -26,6 +47,10 @@ void shell_loop(char** env){
         }
 
         argc.push_back(nullptr);
+
+        if(argc[0] !=NULL){
+            shell_built(argc,env,intial_directory);
+        }
         
     }
 }
